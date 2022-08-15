9e14bba8734e4287030c5a72151f1a8637d3daf9
MandiriTraining <111072208+MandiriTraining@users.noreply.github.com>
2022-08-15 12:45:37 +0700

    push

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.htpp import HttpResponse
 
 def index(request):
-    return HttpResponse("Belajar Django Slur")
+   #return HttpResponse("Belajar Django Slur")
+   return render(request, 'landing_page.html')
 # Create your views here.
